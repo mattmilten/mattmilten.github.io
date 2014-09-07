628893ace231419a20d48c315a07968491ee2f25
Matthias Miltenberger <miltenberger@zib.de>
2014-09-07 20:44:20 +0200

    don't publish gitignore and export script

--- export-www.sh
+++ export-www.sh
@@ -17,6 +17,6 @@ echo "pushing repository..."
 git push || exit
 
 echo "syncing $PAGE..."
-$COPY $COPYFLAGS --exclude=.git --exclude-from=.gitignore . $WWWDIR
+$COPY $COPYFLAGS --exclude=.git --exclude=.gitignore --exclude=export-www.sh --exclude-from=.gitignore . $WWWDIR
 
 exit
